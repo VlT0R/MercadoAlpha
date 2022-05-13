bd30983822a09f02c0faffdaa458325b9630c544
Murilo Oliveira <0220482112006@LAB1MAQ09>
2022-05-13 16:12:50 -0300

    tela de criar tela de editar tela de delete

--- MercadoAlpha/Controllers/FuncionariosController.cs
+++ MercadoAlpha/Controllers/FuncionariosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MercadoAlpha.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,10 +10,15 @@ namespace MercadoAlpha.Controllers
 {
     public class FuncionariosController : Controller
     {
-        // GET: FuncionariosController
+        private readonly Contexto db;
+        public FuncionariosController(Contexto contexto)
+        {
+            db = contexto;
+        }
+        
         public ActionResult Index()
         {
-            return View();
+            return View(db.FUNCIONARIOS.ToList());
         }
 
         // GET: FuncionariosController/Details/5
@@ -30,10 +36,12 @@ namespace MercadoAlpha.Controllers
         // POST: FuncionariosController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Funcionario collection)
         {
             try
             {
+                db.FUNCIONARIOS.Add(collection);
+                db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -45,16 +53,18 @@ namespace MercadoAlpha.Controllers
         // GET: FuncionariosController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            return View(db.FUNCIONARIOS.Where(a => a.Id == id).FirstOrDefault());
         }
 
         // POST: FuncionariosController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Funcionario dadosTela)
         {
             try
             {
+                db.FUNCIONARIOS.Update(dadosTela);
+                db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -63,13 +73,15 @@ namespace MercadoAlpha.Controllers
             }
         }
 
-        // GET: FuncionariosController/Delete/5
+       
         public ActionResult Delete(int id)
         {
-            return View();
+            db.FUNCIONARIOS.Remove(db.FUNCIONARIOS.Where(a => a.Id == id).FirstOrDefault());
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        // POST: FuncionariosController/Delete/5
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
