909b2de2807e65d06c32ef4dac3a5c7b2f34b696
0220482112037 <0220482112037@LAB1MAQ07>
2022-05-13 14:07:35 -0300

    Tela Produtos

---
+++ MercadoAlpha/Models/Produtos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MercadoAlpha.Models
+{
+    public class Produtos
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal UnidadeDeMedida { get; set; }
+        public DateTime DataValidade { get; set; }
+        public int QtdEstoque { get; set; }
+    }
+}
