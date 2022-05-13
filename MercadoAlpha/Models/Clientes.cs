23621c16933f0b1a7c8b63aca5ab6e0d36ddaf32
0220482112005 <0220482112005@LAB1MAQ08>
2022-05-13 14:01:46 -0300

    Telas clientes

---
+++ MercadoAlpha/Models/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MercadoAlpha.Models
+{
+    public class Clientes
+    {
+        public int nome { get; set; }
+        public int datanascimento { get; set; }
+        public int idade { get; set; }
+        public char sexo { get; set; }
+        public int telefone { get; set; }
+        public char email { get; set; }
+        public char endereco { get; set; }
+
+
+    }
+}
