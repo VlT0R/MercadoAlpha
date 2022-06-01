0f0f3f9841c0ad34b0743b07e7554195848de7cd
VITOR AMARAL <vitor.amaral2@fatec.sp.gov.br>
2022-06-01 15:28:52 -0300

    arrumar telas final 0106

---
+++ MercadoAlpha/Migrations/20220601171609_teste87.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace MercadoAlpha.Migrations
+{
+    public partial class teste87 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CLIENTES",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    nomesocial = table.Column<string>(type: "text", nullable: true),
+                    datanascimento = table.Column<DateTime>(type: "datetime", nullable: false),
+                    idade = table.Column<int>(type: "int", nullable: false),
+                    sexo = table.Column<string>(type: "text", nullable: true),
+                    telefone = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    endereco = table.Column<string>(type: "text", nullable: true),
+                    cep = table.Column<string>(type: "text", nullable: true),
+                    cpf = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CLIENTES", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FUNCIONARIOS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    nomesocial = table.Column<string>(type: "text", nullable: true),
+                    cargo = table.Column<string>(type: "text", nullable: true),
+                    datanasc = table.Column<DateTime>(type: "datetime", nullable: false),
+                    sexo = table.Column<string>(type: "text", nullable: true),
+                    endereco = table.Column<string>(type: "text", nullable: true),
+                    telefone = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    rg = table.Column<string>(type: "text", nullable: true),
+                    cep = table.Column<string>(type: "text", nullable: true),
+                    cpf = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FUNCIONARIOS", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PRODUTOS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UnidadeMedida = table.Column<string>(type: "text", nullable: true),
+                    Marca = table.Column<string>(type: "text", nullable: true),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Codigo = table.Column<string>(type: "text", nullable: true),
+                    DataValidade = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    QtdEstoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PRODUTOS", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "USUARIOS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Senha = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USUARIOS", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CLIENTES");
+
+            migrationBuilder.DropTable(
+                name: "FUNCIONARIOS");
+
+            migrationBuilder.DropTable(
+                name: "PRODUTOS");
+
+            migrationBuilder.DropTable(
+                name: "USUARIOS");
+        }
+    }
+}
