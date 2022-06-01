fc784337e7ddc1d1dfb6659a80dff3903afc54c5
VITOR AMARAL <vitor.amaral2@fatec.sp.gov.br>
2022-05-31 21:15:45 -0300

    tela pesquisar em tudo

---
+++ MercadoAlpha/Migrations/20220531230340_3105noite.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MercadoAlpha.Migrations
+{
+    public partial class _3105noite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
