1cb8619de852ef53c2a7fefb7e18d14e0023db55
JOAO FERMINO <joao.fermino@fatec.sp.gov.br>
2022-06-07 15:39:15 -0300

    tela usuario back to list

---
+++ MercadoAlpha/Migrations/20220607182152_joao0706.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MercadoAlpha.Migrations
+{
+    public partial class joao0706 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
